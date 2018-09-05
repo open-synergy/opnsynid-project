9ea7fed2b23f4c46b6402e738a8ad2cdd13760aa
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-05 11:21:22 +0700

    [8.0.1.0.0][project_task_code_by_project]

---
+++ project_task_code_by_project/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Task Code By Project",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project_task_code"
+    ],
+    "data": [
+        "views/project_view.xml"
+    ],
+}
