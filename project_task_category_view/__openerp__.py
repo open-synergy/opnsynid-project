11a637d5d93e6b810781ace635a62fe5d21471ef
Andhitia Rama <andhitia.r@gmail.com>
2018-11-28 08:27:20 +0700

    [8.0.1.0.0] project_task_category_view

---
+++ project_task_category_view/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "View for Task Category",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project_task_category"
+    ],
+    "data": [
+        "views/project_category_main_views.xml"
+    ],
+}
