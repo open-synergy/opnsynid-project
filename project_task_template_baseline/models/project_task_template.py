9e6d154ae4385e2aa2b7df72b3a2d5a311462c5c
Andhitia Rama <andhitia.r@gmail.com>
2018-12-02 19:03:02 +0700

    [8.0.1.0.0] project_task_template_baseline

---
+++ project_task_template_baseline/models/project_task_template.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ProjectTaskTemplate(models.Model):
+    _inherit = "project.task_template"
+
+    start_schedule_base_on = fields.Selection(
+        string="Start Schedule Based On",
+        selection=[
+            ("manual", "Manual"),
+            ("task_start", "Task Baseline Start"),
+            ("task_finish", "Task Baseline Finish"),
+            ("project_start", "Project Baseline Start"),
+            ("project_finish", "Project Baseline Finish"),
+        ],
+        required=True,
+        default="manual",
+    )
+    baseline_start_task_id = fields.Many2one(
+        string="Task Based Schedule",
+        comodel_name="project.task_template",
+    )
+    finish_schedule_base_on = fields.Selection(
+        string="Finish Schedule Based On",
+        selection=[
+            ("manual", "Manual"),
+            ("task_start", "Task Baseline Start"),
+            ("task_finish", "Task Baseline Finish"),
+            ("project_start", "Project Baseline Start"),
+            ("project_finish", "Project Baseline Finish"),
+        ],
+        required=True,
+        default="manual",
+    )
+    baseline_finish_task_id = fields.Many2one(
+        string="Task Based Schedule",
+        comodel_name="project.task_template",
+    )
+    start_offset = fields.Float(
+        string="Baseline Start Offset",
+    )
+    start_offset_uom_id = fields.Many2one(
+        string="Start Offset UoM",
+        comodel_name="product.uom",
+    )
+    finish_offset = fields.Float(
+        string="Baseline Finish Offset",
+    )
+    finish_offset_uom_id = fields.Many2one(
+        string="Finish Offset UoM",
+        comodel_name="product.uom",
+    )
