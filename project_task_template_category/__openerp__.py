e1746b25c1570a766d99d461692e4b6f70e5f5e4
Andhitia Rama <andhitia.r@gmail.com>
2018-12-02 19:04:15 +0700

    [8.0.1.0.0] project_task_template_category

---
+++ project_task_template_category/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Project Template - Task Category Integration",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "project_task_template",
+        "project_task_category",
+    ],
+    "data": [
+        "views/project_template_views.xml",
+    ],
+}
---
+++ project_task_template_category/models/project_task_template.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class ProjectTaskTemplate(models.Model):
+    _inherit = "project.task_template"
+
+    categ_id = fields.Many2one(
+        string="Category",
+        comodel_name="project.category.main",
+    )
+
+    @api.multi
+    def _prepare_task_data(self, project_id):
+        _super = super(ProjectTaskTemplate, self)
+        result = _super._prepare_task_data(project_id)
+        result.update({
+            "categ_id": self.categ_id and
+            self.categ_id.id or False
+        })
+        return result
