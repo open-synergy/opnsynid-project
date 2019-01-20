c8cbd095570c882aa3fefcad719b9dd1ebae498a
Andhitia Rama <andhitia.r@gmail.com>
2019-01-20 08:13:41 +0700

    [8.0.1.0.0] project_team_member_responsibility

---
+++ project_team_member_responsibility/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Project Team Member Responsibility",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "analytic_user_function",
+        "project",
+    ],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+}
