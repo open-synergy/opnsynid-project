ef1979743a9359d4d9ce8784169021e20f23f00c
Andhitia Rama <andhitia.r@gmail.com>
2019-03-11 09:39:04 +0700

    [8.0.1.7.0] project_baseline
    * Add group by baseline start and baseline finish on project and task search view

--- project_baseline/__openerp__.py
+++ project_baseline/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Project Baseline",
-    "version": "8.0.1.6.0",
+    "version": "8.0.1.7.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "category": "Project Management",
