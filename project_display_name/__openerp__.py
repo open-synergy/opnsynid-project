9744963ec390558a2dac06fd0357e9644c53487c
mikevhe18 <viriyananda.michael@gmail.com>
2019-05-27 17:21:43 +0700

    [8.0.1.0.0][project_display_name]

---
+++ project_display_name/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Display Name",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project"
+    ],
+    "data": [
+    ],
+}
