105dfbbcdcb25e86c36dbba8c5db091a26e232e5
Andhitia Rama <andhitia.r@gmail.com>
2019-06-07 10:19:49 +0700

    [8.0.1.1.0] project_task_template_baseline
    * Adjust to change in b3fb459bb1cde1eb4ca86815876e469efcc975fb
    * Remove option to select baseline based on project

--- project_task_template_baseline/models/project_task_template.py
+++ project_task_template_baseline/models/project_task_template.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
@@ -14,8 +14,6 @@ class ProjectTaskTemplate(models.Model):
             ("manual", "Manual"),
             ("task_start", "Task Baseline Start"),
             ("task_finish", "Task Baseline Finish"),
-            ("project_start", "Project Baseline Start"),
-            ("project_finish", "Project Baseline Finish"),
         ],
         required=True,
         default="manual",
@@ -30,8 +28,6 @@ class ProjectTaskTemplate(models.Model):
             ("manual", "Manual"),
             ("task_start", "Task Baseline Start"),
             ("task_finish", "Task Baseline Finish"),
-            ("project_start", "Project Baseline Start"),
-            ("project_finish", "Project Baseline Finish"),
         ],
         required=True,
         default="manual",
