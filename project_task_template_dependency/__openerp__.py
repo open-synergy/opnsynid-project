4394aa00dadfb7709f9c0ef57fe1eb798655d386
Andhitia Rama <andhitia.r@gmail.com>
2019-06-07 19:47:32 +0700

    [8.0.1.1.0] project_task_template_dependency
    * Adjust to change in b3fb459bb1cde1eb4ca86815876e469efcc975fb

--- project_task_template_dependency/__openerp__.py
+++ project_task_template_dependency/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Project Template - Dependency Integration",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "category": "Project Management",
@@ -18,5 +18,6 @@
     "data": [
         "security/ir.model.access.csv",
         "views/project_template_views.xml",
+        "views/project_task_template_views.xml",
     ],
 }
