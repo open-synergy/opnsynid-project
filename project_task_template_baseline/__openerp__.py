f27be215146f2b7a91231dc7677b9b2cafab70ae
Andhitia Rama <andhitia.r@gmail.com>
2019-06-07 10:19:49 +0700

    [8.0.1.1.0] project_task_template_baseline
    * Adjust to change in b3fb459bb1cde1eb4ca86815876e469efcc975fb
    * Remove option to select baseline based on project

--- project_task_template_baseline/__openerp__.py
+++ project_task_template_baseline/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2018-2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Project Template - Baseline Integration",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "category": "Project Management",
@@ -17,5 +17,6 @@
     ],
     "data": [
         "views/project_template_views.xml",
+        "views/project_task_template_views.xml",
     ],
 }
