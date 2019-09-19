656a76c61fb64e49a9d4df0d2bbcbd29ec00f1e5
mikevhe18 <viriyananda.michael@gmail.com>
2019-09-19 16:22:15 +0700

    [8.0.1.0.0][project_workflow_policy]

---
+++ project_workflow_policy/models/project_project.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class ProjectProject(models.Model):
+    _name = "project.project"
+    _inherit = [
+        "project.project",
+        "base.workflow_policy_object",
+    ]
+
+    @api.multi
+    @api.depends(
+        "project_template_id",
+    )
+    def _compute_policy(self):
+        _super = super(ProjectProject, self)
+        _super._compute_policy()
+
+    reopen_ok = fields.Boolean(
+        string="Can Re-Open Project",
+        compute="_compute_policy",
+        store=False,
+    )
+    close_ok = fields.Boolean(
+        string="Can Close Project",
+        compute="_compute_policy",
+        store=False,
+    )
+    pending_ok = fields.Boolean(
+        string="Can Pending",
+        compute="_compute_policy",
+        store=False,
+    )
+    set_template_ok = fields.Boolean(
+        string="Can Set as Template",
+        compute="_compute_policy",
+        store=False,
+    )
+    new_ok = fields.Boolean(
+        string="Can New Project Based on Template",
+        compute="_compute_policy",
+        store=False,
+    )
+    reset_ok = fields.Boolean(
+        string="Can Reset as Project",
+        compute="_compute_policy",
+        store=False,
+    )
+    cancel_ok = fields.Boolean(
+        string="Can Cancel",
+        compute="_compute_policy",
+        store=False,
+    )
