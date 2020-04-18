d3060db142b195cce85e07d5222e5be721800135
Andhitia Rama <andhitia.r@gmail.com>
2020-04-18 17:39:03 +0700

    [8.0.1.0.0] project_task_analytic_account

---
+++ project_task_analytic_account/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Add and Store Analytic Account on Task",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project"
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+}
---
+++ project_task_analytic_account/models/project_task.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ProjectTask(models.Model):
+    _name = "project.task"
+    _inherit = "project.task"
+
+    analytic_account_id = fields.Many2one(
+        string="Analytic Account",
+        comodel_name="account.analytic.account",
+        related="project_id.analytic_account_id",
+    )
