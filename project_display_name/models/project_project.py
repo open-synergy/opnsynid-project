c0a7a43839810c56c3521ac17e785317a4d31f73
mikevhe18 <viriyananda.michael@gmail.com>
2020-04-21 08:20:10 +0700

    [8.0.1.0.1][project_display_name][T0037] Adds method to search data according to code

--- project_display_name/models/project_project.py
+++ project_display_name/models/project_project.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, api
@@ -18,3 +19,19 @@ class ProjectProject(models.Model):
                 name = "%s" % (rec.name)
             result.append((rec.id, name))
         return result
+
+    @api.model
+    def name_search(self, name="", args=None, operator="ilike", limit=100):
+        args = list(args or [])
+        if name:
+            search_name = name
+            if operator != "=":
+                search_name = "%s%%" % name
+            project_ids = self.search(
+                [("code", operator, search_name)] + args,
+                limit=limit
+            )
+            if project_ids.ids:
+                return project_ids.name_get()
+        return super(ProjectProject, self)\
+            .name_search(name=name, args=args, operator=operator, limit=limit)
