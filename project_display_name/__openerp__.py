eb9a13cff5a3422014eba08b1c7c42caa4db6ddf
mikevhe18 <viriyananda.michael@gmail.com>
2020-04-21 08:20:10 +0700

    [8.0.1.0.1][project_display_name][T0037] Adds method to search data according to code

--- project_display_name/__openerp__.py
+++ project_display_name/__openerp__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Display Name",
-    "version": "8.0.1.0.0",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "version": "8.0.1.0.1",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
