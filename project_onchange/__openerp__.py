fb315627950ee576e1571c5902091e4203ddb84a
Andhitia Rama <andhitia.r@gmail.com>
2020-08-05 23:30:40 +0700

    [8.0.1.0.0] project_onchange

---
+++ project_onchange/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Onchange New API",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
