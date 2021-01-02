cd02e9f79469e10d975b8fc04990d7238d97c1c5
Andhitia Rama <andhitia.r@gmail.com>
2021-01-02 20:24:28 +0700

    [project_completion_revenue_recognition] 8.0.1.0.1
    * Unlink move when project reopen

--- project_completion_revenue_recognition/__openerp__.py
+++ project_completion_revenue_recognition/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Recognize Revenue When Project Complete",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
