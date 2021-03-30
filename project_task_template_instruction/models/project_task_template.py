6df71b210c77fdc12014fbb0a22c874eff255942
mikevhe18 <viriyananda.michael@gmail.com>
2021-03-30 12:25:18 +0700

    [project_task_template_instruction] 8.0.1.0.0

---
+++ project_task_template_instruction/models/project_task_template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ProjectTaskTemplate(models.Model):
+    _inherit = "project.task_template"
+
+    instruction_ids = fields.One2many(
+        string="Instruction",
+        comodel_name="project.task_tmpl_main_instruction",
+        inverse_name="task_template_id",
+    )
