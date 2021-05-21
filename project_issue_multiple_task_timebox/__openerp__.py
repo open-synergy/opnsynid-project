3e34741c23b64ef4c9fa5fa304749e4d7f4b65b4
Andhitia Rama <andhitia.r@gmail.com>
2021-05-21 21:04:05 +0700

    [project_issue_multiple_task_timebox/] 8.0.1.0.0

---
+++ project_issue_multiple_task_timebox/__openerp__.py
@@ -0,0 +1,24 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Project Issue Multiple Task Timebox",
+    "version": "8.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "project_issue_multiple_task",
+        "project_timebox",
+    ],
+    "data": [
+        "views/project_issue_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
