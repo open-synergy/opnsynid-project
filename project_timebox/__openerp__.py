e1c26bde55d70cdf784d1e22e663fadf3ae1bb62
azmimr67@gmail.com <azmimr67@gmail.com>
2021-05-24 15:29:01 +0700

    [project_timebox] 8.0.1.3.1
    Fix #46

--- project_timebox/__openerp__.py
+++ project_timebox/__openerp__.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Task Timebox",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.3.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
