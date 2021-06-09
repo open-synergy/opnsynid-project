cacaac33889c36e8094fef171709a7d07c047955
azmimr67@gmail.com <azmimr67@gmail.com>
2021-06-09 13:21:06 +0700

    [project_issue_display_name] 8.0.1.0.0 T/21/003160 - Issue #57

---
+++ project_issue_display_name/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Issue Display Name",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project_issue",
+        "project_issue_code",
+    ],
+    "data": [
+    ],
+}
