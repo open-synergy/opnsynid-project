c40a79c96e93d9bd0d7150e9b287147e8ec3ae0f
Andhitia Rama <andhitia.r@gmail.com>
2021-07-02 23:14:25 +0700

    [project_timebox] 8.0.1.6.0

--- project_timebox/__openerp__.py
+++ project_timebox/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Task Timebox",
-    "version": "8.0.1.5.1",
+    "version": "8.0.1.6.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
@@ -12,10 +12,12 @@
         "project",
     ],
     "data": [
-        "demo/timebox_demo.xml",
         "security/ir.model.access.csv",
         "views/project_timebox_views.xml",
         "views/project_task_views.xml",
+    ],
+    "demo": [
+        "demo/timebox_demo.xml",
     ],
     "images": [
         "static/description/banner.png",
