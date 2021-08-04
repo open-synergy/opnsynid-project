3662aceeb1912c1e19dfd4e10ba7dfd667efd7d2
Nurazmi <azmimr67@gmail.com>
2021-08-04 12:21:17 +0700

    [8.0.1.0.0] project_assignment_letter
    Fix #67

---
+++ project_assignment_letter/models/project_assignment_letter.py
@@ -0,0 +1,251 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
+
+
+class ProjectAssignmentLetter(models.Model):
+    _name = "project.assignment_letter"
+    _description = "Project Assignment Letter"
+    _inherit = [
+        "mail.thread",
+        "tier.validation",
+        "base.sequence_document",
+        "base.workflow_policy_object",
+        "base.cancel.reason_common",
+    ]
+    _state_from = ["draft", "confirm"]
+    _state_to = ["valid"]
+
+    @api.multi
+    @api.depends(
+        "project_id",
+    )
+    def _compute_policy(self):
+        _super = super(ProjectAssignmentLetter, self)
+        _super._compute_policy()
+
+    name = fields.Char(
+        string="# Document",
+        default="/",
+        required=True,
+        copy=False,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    project_id = fields.Many2one(
+        string="Project",
+        comodel_name="project.project",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    signee_id = fields.Many2one(
+        string="Signed By",
+        comodel_name="res.users",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    date = fields.Date(
+        string="Date",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        required=True,
+        readonly=True,
+        related="project_id.company_id",
+    )
+    task_ids = fields.One2many(
+        string="Task",
+        comodel_name="project.task",
+        inverse_name="assignment_letter_id",
+        readonly=False,
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("draft", "Draft"),
+            ("confirm", "Waiting for Approval"),
+            ("valid", "Valid"),
+            ("cancel", "Cancelled"),
+        ],
+        copy=False,
+        default="draft",
+        required=True,
+        readonly=True,
+    )
+    confirm_ok = fields.Boolean(
+        string="Can Confirm",
+        compute="_compute_policy",
+    )
+    restart_validation_ok = fields.Boolean(
+        string="Can Restart Validation",
+        compute="_compute_policy",
+    )
+    valid_ok = fields.Boolean(
+        string="Can Valid",
+        compute="_compute_policy",
+    )
+    cancel_ok = fields.Boolean(
+        string="Can Cancel",
+        compute="_compute_policy",
+    )
+    restart_ok = fields.Boolean(
+        string="Can Restart",
+        compute="_compute_policy",
+    )
+    confirm_date = fields.Datetime(
+        string="Confirmation Date",
+        readonly=True,
+        copy=False,
+    )
+    confirm_user_id = fields.Many2one(
+        string="Confirmed By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    valid_date = fields.Datetime(
+        string="Validation Date",
+        readonly=True,
+        copy=False,
+    )
+    valid_user_id = fields.Many2one(
+        string="Valid By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    cancel_date = fields.Datetime(
+        string="Cancel Date",
+        readonly=True,
+        copy=False,
+    )
+    cancel_user_id = fields.Many2one(
+        string="Cancelled By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+
+    @api.multi
+    def action_confirm(self):
+        for record in self:
+            record.write(record._prepare_confirm_data())
+            record.request_validation()
+
+    @api.multi
+    def action_valid(self):
+        for record in self:
+            record.write(record._prepare_valid_data())
+
+    @api.multi
+    def action_cancel(self):
+        for record in self:
+            record.write(record._prepare_cancel_data())
+
+    @api.multi
+    def action_restart(self):
+        for record in self:
+            record.write(record._prepare_restart_data())
+
+    @api.multi
+    def _prepare_confirm_data(self):
+        self.ensure_one()
+        return {
+            "state": "confirm",
+            "confirm_date": fields.Datetime.now(),
+            "confirm_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_valid_data(self):
+        self.ensure_one()
+        sequence = self._create_sequence()
+        return {
+            "state": "valid",
+            "name": sequence,
+            "valid_date": fields.Datetime.now(),
+            "valid_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_cancel_data(self):
+        self.ensure_one()
+        return {
+            "state": "cancel",
+            "cancel_date": fields.Datetime.now(),
+            "cancel_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_restart_data(self):
+        self.ensure_one()
+        return {
+            "state": "draft",
+            "confirm_date": False,
+            "confirm_user_id": False,
+            "valid_date": False,
+            "valid_user_id": False,
+            "cancel_date": False,
+            "cancel_user_id": False,
+        }
+
+    @api.multi
+    def unlink(self):
+        strWarning = _("You can only delete data on draft state")
+        for record in self:
+            if record.state != "draft":
+                if not self.env.context.get("force_unlink", False):
+                    raise UserError(strWarning)
+        _super = super(ProjectAssignmentLetter, self)
+        _super.unlink()
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            if record.name == "/":
+                name = "*" + str(record.id)
+            else:
+                name = record.name
+            result.append((record.id, name))
+        return result
+
+    @api.multi
+    def validate_tier(self):
+        _super = super(ProjectAssignmentLetter, self)
+        _super.validate_tier()
+        for record in self:
+            if record.validated:
+                record.action_valid()
+
+    @api.multi
+    def restart_validation(self):
+        _super = super(ProjectAssignmentLetter, self)
+        _super.restart_validation()
+        for record in self:
+            record.request_validation()
