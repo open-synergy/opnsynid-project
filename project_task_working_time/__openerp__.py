de28b401330de781b05602b5a0b0372fbcbff475
mikevhe18 <viriyananda.michael@gmail.com>
2022-01-25 17:42:23 +0700

    [project_task_working_time] 8.0.1.0.0

---
+++ project_task_working_time/__openerp__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Project Task Working Time",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "timesheet_task",
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+    "demo": [],
+}
