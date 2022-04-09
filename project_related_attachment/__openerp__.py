85d82f4e9786c983e3ef33dd053cd43f76c9cd0c
Andhitia Rama <andhitia.r@gmail.com>
2022-04-09 11:17:50 +0700

    [project_related_attachment] 8.0.1.0.0

---
+++ project_related_attachment/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Related Attachment",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "project",
+        "ssi_related_attachment_mixin",
+    ],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+}
