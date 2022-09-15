1a46be2ab46acb2d870d397e523e8d82b0118a7c
Andhitia Rama <andhitia.r@gmail.com>
2022-09-15 21:09:24 +0700

    [ssi_task_work_log] 14.0.1.0.0

---
+++ ssi_task_work_log/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+{
+    "name": "Task - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_task_type",
+        "ssi_project_work_log",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
