38fe8d569ced47f9d51cac40d80155710dca32f9
Andhitia Rama <andhitia.r@gmail.com>
2022-09-16 07:30:29 +0700

    [ssi_task_project_manager] 14.0.1.0.0

---
+++ ssi_task_project_manager/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Manager Information on Task",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_project",
+    ],
+    "data": [
+        "views/project_task_views.xml",
+    ],
+    "demo": [],
+}
