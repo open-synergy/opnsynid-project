4bf65cf4fac4cc17f9979cc6f019aba22ac90503
Andhitia Rama <andhitia.r@gmail.com>
2022-09-30 15:43:01 +0700

    [ssi_project_assignment] 14.0.1.0.0

---
+++ ssi_project_assignment/__manifest__.py
@@ -0,0 +1,36 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Assignment",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_project",
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_transaction_terminate_mixin",
+        "ssi_duration_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/approval_template_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "views/project_role_views.xml",
+        "views/project_assignment_views.xml",
+        "views/project_project_views.xml",
+    ],
+    "demo": [],
+}
---
+++ ssi_project_assignment/models/project_assignment.py
@@ -0,0 +1,142 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class ProjectAssignment(models.Model):
+    _name = "project.assignment"
+    _inherit = [
+        "mixin.transaction_confirm",
+        "mixin.transaction_cancel",
+        "mixin.transaction_open",
+        "mixin.transaction_done",
+        "mixin.transaction_terminate",
+        "mixin.date_duration",
+    ]
+    _description = "Project Assignment"
+    # Multiple Approval Attribute
+    _approval_from_state = "draft"
+    _approval_to_state = "open"
+    _approval_state = "confirm"
+    _after_approved_method = "action_open"
+
+    # Attributes related to add element on view automatically
+    _automatically_insert_view_element = True
+    _automatically_insert_open_button = False
+    _automatically_insert_open_policy_fields = False
+
+    # Mixin duration attribute
+    _date_start_readonly = True
+    _date_end_readonly = True
+    _date_start_states_list = ["draft"]
+    _date_start_states_readonly = ["draft"]
+    _date_end_states_list = ["draft"]
+    _date_end_states_readonly = ["draft"]
+
+    # Attributes related to add element on form view automatically
+    _automatically_insert_multiple_approval_page = True
+    _statusbar_visible_label = "draft,confirm,open,done"
+    _policy_field_order = [
+        "confirm_ok",
+        "approve_ok",
+        "reject_ok",
+        "restart_approval_ok",
+        "done_ok",
+        "cancel_ok",
+        "terminate_ok",
+        "restart_ok",
+        "manual_number_ok",
+    ]
+    _header_button_order = [
+        "action_confirm",
+        "action_approve_approval",
+        "action_reject_approval",
+        "action_done",
+        "action_terminate",
+        "%(ssi_transaction_cancel_mixin.base_select_cancel_reason_action)d",
+        "action_restart",
+    ]
+
+    # Attributes related to add element on search view automatically
+    _state_filter_order = [
+        "dom_draft",
+        "dom_confirm",
+        "dom_reject",
+        "dom_open",
+        "dom_done",
+        "dom_terminate",
+        "dom_cancel",
+    ]
+
+    # Sequence attribute
+    _create_sequence_state = "open"
+
+    date_assign = fields.Date(
+        string="Date Assign",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+        },
+    )
+
+    project_id = fields.Many2one(
+        string="Project",
+        comodel_name="project.project",
+        required=True,
+        ondelete="cascade",
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+        },
+    )
+
+    role_id = fields.Many2one(
+        string="Role",
+        comodel_name="project.role",
+        required=True,
+        ondelete="restrict",
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+        },
+    )
+    asignee_id = fields.Many2one(
+        string="Asignee",
+        comodel_name="res.users",
+        required=True,
+        ondelete="restrict",
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+        },
+    )
+    state = fields.Selection(
+        selection=[
+            ("draft", "Draft"),
+            ("confirm", "Waiting for Approval"),
+            ("open", "In Progress"),
+            ("done", "Done"),
+            ("terminate", "Terminate"),
+            ("cancel", "Cancelled"),
+        ],
+    )
+
+    @api.model
+    def _get_policy_field(self):
+        res = super(ProjectAssignment, self)._get_policy_field()
+        policy_field = [
+            "confirm_ok",
+            "approve_ok",
+            "done_ok",
+            "terminate_ok",
+            "cancel_ok",
+            "reject_ok",
+            "restart_ok",
+            "restart_approval_ok",
+            "manual_number_ok",
+        ]
+        res += policy_field
+        return res
