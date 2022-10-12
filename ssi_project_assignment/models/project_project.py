045ffa001cabf4922f8b092761e8c0d31df66438
Andhitia Rama <andhitia.r@gmail.com>
2022-10-12 16:17:21 +0700

    [ssi_project_assignment] 14.0.1.4.0
    * Separate all team and active team

--- ssi_project_assignment/models/project_project.py
+++ ssi_project_assignment/models/project_project.py
@@ -26,12 +26,25 @@ class ProjectProject(models.Model):
             result = record.assignment_ids.filtered(
                 lambda r: r.state in ["open", "done", "terminate"]
             ).mapped("asignee_id")
+            result_active = record.assignment_ids.filtered(
+                lambda r: r.state == "open"
+            ).mapped("asignee_id")
             record.team_ids = result.ids
+            record.active_team_ids = result_active.ids
 
     team_ids = fields.Many2many(
         string="Team(s)",
         comodel_name="res.users",
         relation="rel_project_to_team",
+        column1="project_id",
+        column2="user_id",
+        compute="_compute_team_ids",
+        store=True,
+    )
+    active_team_ids = fields.Many2many(
+        string="Active Team(s)",
+        comodel_name="res.users",
+        relation="rel_project_to_active_team",
         column1="project_id",
         column2="user_id",
         compute="_compute_team_ids",
