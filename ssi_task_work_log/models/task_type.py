91004923a25717d08925857dcdd5bb4a69990495
Andhitia Rama <andhitia.r@gmail.com>
2022-12-16 05:19:31 +0700

    [ssi_task_work_log] 14.0.1.2.0

--- ssi_task_work_log/models/task_type.py
+++ ssi_task_work_log/models/task_type.py
@@ -14,3 +14,20 @@ class TaskType(models.Model):
     work_estimation = fields.Float(
         string="Work Estimation",
     )
+    match_aa_partner = fields.Boolean(
+        string="Match AA Partner",
+    )
+    work_log_analytic_account_ids = fields.Many2many(
+        string="Work Log Analytic Account",
+        comodel_name="account.analytic.account",
+        relation="rel_task_type_2_work_log_aa",
+        column1="type_id",
+        column2="analytic_account_id",
+    )
+    work_log_analytic_group_ids = fields.Many2many(
+        string="Work Log Analytic Group",
+        comodel_name="account.analytic.group",
+        relation="rel_task_type_2_work_log_ag",
+        column1="type_id",
+        column2="analytic_group_id",
+    )
