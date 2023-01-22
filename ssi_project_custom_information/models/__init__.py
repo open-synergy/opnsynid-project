fb03a7dbc74138d135e1274f124fdb75d8a95e77
Andhitia Rama <andhitia.r@gmail.com>
2023-01-23 05:25:32 +0700

    [ssi_project_custom_information] 14.0.1.0.0

---
+++ ssi_project_custom_information/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+from . import (
+    project_project,
+    project_task,
+)
---
+++ ssi_project_custom_information/models/project_project.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class ProjectProject(models.Model):
+    _name = "project.project"
+    _inherit = [
+        "project.project",
+        "mixin.custom_info",
+    ]
