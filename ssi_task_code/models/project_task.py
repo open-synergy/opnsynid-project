c309eacb3a1ea10fd47312a49f02f22bbdb4425e
mikevhe18 <viriyananda.michael@gmail.com>
2023-02-01 05:53:41 +0700

    [ssi_task_code] 14.0.1.0.1

--- ssi_task_code/models/project_task.py
+++ ssi_task_code/models/project_task.py
@@ -2,6 +2,7 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from odoo import _, api, fields, models
+from odoo.osv import expression
 
 
 class ProjectTask(models.Model):
@@ -44,3 +45,16 @@ class ProjectTask(models.Model):
             name = "[{}] {}".format(rec.code, task[1])
             new_result.append((rec.id, name))
         return new_result
+
+    @api.model
+    def _name_search(
+        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
+        if operator in ("ilike", "like", "=", "=like", "=ilike"):
+            args = expression.AND(
+                [args or [], ["|", ("code", operator, name), ("name", operator, name)]]
+            )
+            return self._search(args, limit=limit, access_rights_uid=name_get_uid)
+        return super(ProjectTask, self)._name_search(
+            name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid
+        )
