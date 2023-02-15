8f700d23ba785df1b808cdc1046b05f5d46fb8f1
mikevhe18 <viriyananda.michael@gmail.com>
2023-02-15 10:07:26 +0700

    [ssi_project_code] 14.0.1.0.0

---
+++ ssi_project_code/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Project Code",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_project",
+    ],
+    "data": [
+        "views/project_project_views.xml",
+    ],
+    "demo": [],
+}
