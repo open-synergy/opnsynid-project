efcf0a103c71b7ae39d224c3285069d6c0795677
mikevhe18 <viriyananda.michael@gmail.com>
2023-05-29 13:20:10 +0700

    [ssi_project] 14.0.2.11.1 QC

--- ssi_project/models/project_project.py
+++ ssi_project/models/project_project.py
@@ -2,49 +2,58 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import fields, models
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = "project.project"
 
     state = fields.Selection(
-        string='State',
+        string="State",
         selection=[
-            ('draft', 'New'),
-            ('open', 'In Progress'),
-            ('done', 'Done'),
-            ('pending', 'Pending'),
-            ('cancel', 'Cancel'),
-        ], default='draft', copy=False)
+            ("draft", "New"),
+            ("open", "In Progress"),
+            ("done", "Done"),
+            ("pending", "Pending"),
+            ("cancel", "Cancel"),
+        ],
+        default="draft",
+        copy=False,
+    )
+
+    def _prepare_confirm_data(self):
+        return {"state": "open"}
 
     def action_confirm(self):
-        for rec in self.filtered(lambda p: p.state == 'draft'):
-            rec.write({
-                'state': 'open',
-            })
+        for rec in self.filtered(lambda p: p.state == "draft"):
+            rec.write(rec._prepare_confirm_data())
+
+    def _prepare_done_data(self):
+        return {"state": "done"}
 
     def action_done(self):
-        for rec in self.filtered(lambda p: p.state == 'open'):
-            rec.write({
-                'state': 'done',
-            })
+        for rec in self.filtered(lambda p: p.state in ["open", "pending"]):
+            rec.write(rec._prepare_done_data())
+
+    def _prepare_pending_data(self):
+        return {"state": "pending"}
 
     def action_pending(self):
-        for rec in self.filtered(lambda p: p.state == 'open'):
-            rec.write({
-                'state': 'pending',
-            })
+        for rec in self.filtered(lambda p: p.state == "open"):
+            rec.write(rec._prepare_pending_data())
+
+    def _prepare_cancel_data(self):
+        return {"state": "cancel"}
 
     def action_cancel(self):
-        for rec in self.filtered(lambda p: p.state in ['draft', 'open', 'pending', 'done']):
-            rec.write({
-                'state': 'cancel',
-            })
+        for rec in self.filtered(
+            lambda p: p.state in ["draft", "open", "pending", "done"]
+        ):
+            rec.write(rec._prepare_cancel_data())
+
+    def _prepare_draft_data(self):
+        return {"state": "draft"}
 
     def action_draft(self):
-        for rec in self.filtered(lambda p: p.state == 'cancel'):
-            rec.write({
-                'state': 'draft',
-            })
+        for rec in self.filtered(lambda p: p.state == "cancel"):
+            rec.write(rec._prepare_draft_data())
