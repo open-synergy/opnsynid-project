3b08add717b87a2db88e4e4510a92b7e17ba908c
Andhitia Rama <andhitia.r@gmail.com>
2023-06-05 16:12:12 +0700

    [ssi_project] 14.0.2.12.0

--- ssi_project/__manifest__.py
+++ ssi_project/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Project Extension",
-    "version": "14.0.2.11.1",
+    "version": "14.0.2.12.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
