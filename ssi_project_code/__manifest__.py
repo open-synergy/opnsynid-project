22f2b222cf7e48bd063a4efa024d86b5453132cf
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-24 12:16:02 +0700

    Publish to PyPi

--- ssi_project_code/__manifest__.py
+++ ssi_project_code/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Project Code",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
