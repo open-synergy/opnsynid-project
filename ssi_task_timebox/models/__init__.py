e4a0f43053ad28c4784a3f993d5cbd0452d4279d
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-08 07:43:50 +0700

    [ssi_task_timebox] Change License To AGPL-3

--- ssi_task_timebox/models/__init__.py
+++ ssi_task_timebox/models/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
     task_timebox,
