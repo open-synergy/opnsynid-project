1dccd24372e1237f61adc34ddf74f9e68be3f40d
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-08 07:44:31 +0700

    [ssi_task_mixin] Change License To AGPL-3

--- ssi_task_mixin/__manifest__.py
+++ ssi_task_mixin/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Task Mixin",
     "version": "14.0.1.1.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_task_timebox",
