f9c4ed8ec51002c070abdddca8f9ed79d3a5f26d
Andhitia Rama <andhitia.r@gmail.com>
2023-09-15 11:01:52 +0700

    [ADD] ssi_task_data_requirement

---
+++ ssi_task_data_requirement/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Task - Data Requirement Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_task_type",
+        "ssi_data_requirement_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
