a0515cf0847b84ae126554bc9d715443641677e4
Andhitia Rama <andhitia.r@gmail.com>
2023-12-25 14:47:06 +0700

    [UPD] ssi_project_template
    * Adjust to baseline mechanism

--- ssi_project_template/models/project_task.py
+++ ssi_project_template/models/project_task.py
@@ -22,3 +22,44 @@ class ProjectTask(models.Model):
 
         if self.template_id and self.template_id.type_id:
             self.type_id = self.template_id.type_id
+
+    def _compute_task_timebox_baseline_from_template(self):
+        self.ensure_one()
+        template = self.template_id
+        data = {}
+
+        if template.baseline_method == "none":
+            data.update(
+                {
+                    "baseline_method": "none",
+                }
+            )
+        elif template.baseline_method == "project":
+            data.update(
+                {
+                    "baseline_method": "project",
+                    "baseline_project_id": self.project_id.id,
+                    "baseline_offset": template.baseline_offset,
+                }
+            )
+        else:
+            criteria = [
+                ("project_id", "=", self.project_id.id),
+                ("template_id", "=", template.baseline_task_template_id.id),
+            ]
+            Task = self.env["project.task"]
+            tasks = Task.search(criteria)
+            data.update(
+                {
+                    "baseline_method": "task",
+                    "baseline_project_id": self.project_id.id,
+                    "baseline_offset": template.baseline_offset,
+                }
+            )
+            if len(tasks) > 0:
+                data.update(
+                    {
+                        "baseline_task_id": tasks[0].id,
+                    }
+                )
+        self.write(data)
