987a14ad06c68536ccf095d6076cbfc1c417e8b5
Miftahussalam <miftah_salam@ymail.com>
2024-02-19 15:34:17 +0700

    [UPD] ssi_project

--- ssi_project/models/mail_compose_message.py
+++ ssi_project/models/mail_compose_message.py
@@ -1,3 +1,7 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
