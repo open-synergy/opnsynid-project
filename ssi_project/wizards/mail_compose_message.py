b4b92a3030a9cc375f7e849623754d8b974f9d28
mikevhe18 <viriyananda.michael@gmail.com>
2024-02-29 10:24:49 +0700

    [UPD] Fixing error when send email

---
+++ ssi_project/wizards/mail_compose_message.py
@@ -0,0 +1,15 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class MailComposeMessage(models.TransientModel):
+    _inherit = "mail.compose.message"
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(MailComposeMessage, self).default_get(fields_list)
+        res.setdefault("partner_ids", False)
+        return res
