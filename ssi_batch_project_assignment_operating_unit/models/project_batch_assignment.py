d0f90cc6b942156a43500481d927236083034a6d
Andhitia Rama <andhitia.r@gmail.com>
2025-01-10 09:17:03 +0700

    [ADD] ssi_batch_project_assignment_operating_unit

---
+++ ssi_batch_project_assignment_operating_unit/models/project_batch_assignment.py
@@ -0,0 +1,19 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProjectBatchAssignment(models.Model):
+    _name = "project.batch_assignment"
+    _inherit = [
+        "project.batch_assignment",
+        "mixin.single_operating_unit",
+    ]
+
+    operating_unit_id = fields.Many2one(
+        related="project_id.operating_unit_id",
+        store=True,
+        default=False,
+    )
