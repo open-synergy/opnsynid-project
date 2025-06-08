55e250fc1aa319eda677cb4ceac8a5759b143e8e
Andhitia Rama <andhitia.r@gmail.com>
2025-06-08 14:13:02 +0700

    [UPD] ssi_project
    * Menambahkan title pd tree deliverable dan phase
    * Memperbaiki trigger perhitungan percent completion

--- ssi_project/models/project_phase.py
+++ ssi_project/models/project_phase.py
@@ -222,7 +222,7 @@ class ProjectPhase(models.Model):
     @api.depends(
         "task_ids",
         "task_ids.phase_id",
-        "task_ids.state",
+        "task_ids.stage_id",
     )
     def _compute_number_of_task(self):
         Task = self.env["project.task"]
@@ -255,7 +255,7 @@ class ProjectPhase(models.Model):
     @api.depends(
         "child_ids",
         "child_ids.completion_percentage",
-        "child_ids.state",
+        "child_ids.stage_id",
         "child_ids.parent_id",
     )
     def _compute_number_of_child(self):
@@ -338,7 +338,7 @@ class ProjectPhase(models.Model):
         "child_ids.parent_id",
         "task_ids",
         "task_ids.phase_id",
-        "task_ids.state",
+        "task_ids.stage_id",
         "task_ids.stage_id",
         "deliverable_ids",
         "deliverable_ids.state",
