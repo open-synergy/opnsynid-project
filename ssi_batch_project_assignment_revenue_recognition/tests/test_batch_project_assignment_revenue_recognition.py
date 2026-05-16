037414de0e7c9c3365fea33be760bf9dbc463da1
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 21:02:51 +0700

    [ADD] ssi_batch_project_assignment_revenue_recognition
    * Add unit tests

---
+++ ssi_batch_project_assignment_revenue_recognition/tests/test_batch_project_assignment_revenue_recognition.py
@@ -0,0 +1,15 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestBatchProjectAssignmentRevenueRecognition(YamlTransactionCase):
+    def test_batch_project_assignment_revenue_recognition(self):
+        self.run_yaml_scenario(
+            "test_data_batch_project_assignment_revenue_recognition.yaml"
+        )
