c8e30dc49de786a2b3b5797e252517e272e7293f
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 21:00:32 +0700

    [ADD] ssi_task_code
    * Add unit tests

--- ssi_task_code/__manifest__.py
+++ ssi_task_code/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Task Code",
-    "version": "14.0.1.3.0",
+    "version": "14.0.1.3.1",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
