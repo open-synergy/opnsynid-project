e8b4b7afe7566740d4faafe8fa9d12e3eed11870
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 21:00:23 +0700

    [ADD] ssi_project_code
    * Add unit tests

--- ssi_project_code/__manifest__.py
+++ ssi_project_code/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project Code",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
